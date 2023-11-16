558b963cba60f4a874b3f58ad1f1517e0344d5de
Arthur <arthurmartins696@gmail.com>
2023-11-16 10:41:19 -0400

    iniciando a tela de cadastro faltando a logica

---
+++ src/app/views/abas/produtos/cadastrar/cadastrar.component.ts
@@ -0,0 +1,120 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Util } from 'src/app/core/util.model';
+import { ModalController } from '@ionic/angular';
+import { OverlayService } from 'src/app/core/service/overlay.service';
+import { DataBaseProvider } from 'src/app/core/service/database';
+import { Produto } from 'src/app/core/model/data-base/produto.model';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.scss'],
+})
+export class CadastrarComponent implements OnInit {
+  @ViewChild('btnVoltar') btnVoltar;
+  produto: Produto;
+  submitted: boolean;
+  consultando: boolean = false;
+  constructor(
+    private overlay: OverlayService,
+    private dados: DataBaseProvider,
+    private modal: ModalController
+  ) {
+    this.submitted = false;
+    this.consultando = false;
+    this.produto = new Produto();
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  dismissModal() {
+    try {
+      this.btnVoltar.voltar();
+    } catch {
+      setTimeout(() => {
+        this.btnVoltar.voltar();
+      }, 500);
+    }
+  }
+
+  ngOnInit() {
+    try {
+      this.consultando = true;
+      const modalState = {
+        modal: true,
+        desc: 'fake state for our modal',
+      };
+      history.pushState(modalState, null);
+    } catch (e) {
+      Util.TratarErro(e);
+      this.consultando = false;
+    }
+  }
+
+  setFocusDocumento() {
+    try {
+      setTimeout(() => {
+        document.getElementById('cpf_cnpj').focus();
+      }, 500);
+    } catch (e) {
+      console.error('setFocusDocumento', e);
+    }
+  }
+
+  ajustarQuantidade(registro: Produto, incremento: number) {
+    if (!registro.qtde_produto) {
+      registro.qtde_produto = 0;
+    }
+    registro.qtde_produto += incremento;
+    if (registro.qtde_produto <= -1) {
+      registro.qtde_produto = null;
+      return;
+    }
+  }
+
+  alterouQuantidadeManualmente(registro: Produto, novoValor) {
+    registro.qtde_produto = novoValor;
+    if (registro.qtde_produto && registro.qtde_produto < 0) {
+      registro.qtde_produto = 1;
+    }
+  }
+
+  async onSalvar() {
+    this.submitted = true;
+    try {
+      this.overlay.showLoading('Cadastrando cliente...');
+      let menorId = await this.dados.getMenorIdTabela('cliente');
+      if (!menorId) {
+        menorId = -1;
+      } else {
+        menorId = --menorId;
+        if (menorId >= 0) {
+          menorId = -1;
+        }
+      }
+
+      const novoProduto = new Produto();
+      novoProduto.id = novoProduto.id_erp = menorId;
+      novoProduto.nome = this.produto.nome.toString().toUpperCase();
+      novoProduto.data_fabricacao = this.produto.data_fabricacao;
+      novoProduto.data_vencimento = this.produto.data_vencimento;
+      novoProduto.qtde_produto = this.produto.qtde_produto;
+      novoProduto.valor_unitario = this.produto.valor_unitario;
+      novoProduto.valor_total = this.produto.valor_total;
+      novoProduto.produto_perecivel = this.produto.produto_perecivel;
+
+      this.dados
+        .setProdutos([novoProduto])
+        .then((result) => {
+          this.overlay.dismissLoadCtrl();
+          this.overlay.notificarSucesso('Produto salvo com sucesso!');
+          this.modal.dismiss(novoProduto);
+        })
+        .catch((e) => {
+          Util.TratarErroEFecharLoading(e, this.overlay);
+        });
+    } catch (e) {
+      Util.TratarErroEFecharLoading(e, this.overlay);
+    }
+  }
+}
---
+++ src/app/views/abas/produtos/financeiro/financeiro.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { fromEvent, Subscription } from 'rxjs';
+import { Produto } from 'src/app/core/model/data-base/produto.model';
+import { ViewCliente } from 'src/app/core/model/data-base/view-cliente.model';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { DataBaseProvider } from 'src/app/core/service/database';
+import { Util } from 'src/app/core/util.model';
+
+@Component({
+  selector: 'app-financeiro',
+  templateUrl: './financeiro.component.html',
+  styleUrls: ['./financeiro.component.scss'],
+})
+export class FinanceiroComponent implements OnInit, OnDestroy {
+  @ViewChild('btnVoltar') btnVoltar;
+  cliente: ViewCliente;
+  produtos: Produto[] = [];
+  consultando: boolean;
+  total_aberto: number;
+  multa: number;
+  juros: number;
+  abaSelecionada: string;
+  private backbuttonSubscription: Subscription;
+  constructor(
+    private dados: DataBaseProvider,
+    public modal: ModalController,
+    auth: AuthService
+  ) {
+    this.consultando = false;
+    this.total_aberto = 0;
+    this.abaSelecionada = 'contas';
+  }
+
+  segmentChanged(ev: any) {
+    this.abaSelecionada = ev.detail.value;
+  }
+
+  ngOnDestroy() {
+    this.backbuttonSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    try {
+      const event = fromEvent(document, 'backbutton');
+      this.backbuttonSubscription = event.subscribe(async () => {
+        try {
+          this.modal.dismiss();
+        } catch {}
+      });
+
+      this.dados
+        .getEmpresaLogada()
+        .then((empresa) => {
+          this.multa = empresa.multa_contas_a_receber_em_atraso;
+          this.juros = empresa.juros_mensal_contas_a_receber_em_atraso;
+        })
+        .catch((e) => {
+          Util.TratarErro(e);
+          this.consultando = false;
+        });
+    } catch (e) {
+      Util.TratarErro(e);
+      this.consultando = false;
+    }
+  }
+
+  getNomeCliente() {
+    return this.cliente.fantasia ? this.cliente.fantasia : this.cliente.razao;
+  }
+}
